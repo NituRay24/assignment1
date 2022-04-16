209848e278e99122a8effa9f5e51fa0dd1c533b2
NituRay24 <raynitu24@gmail.com>
2022-04-17 00:20:07 +0530

    day 1 assignment

---
+++ src/Leapyear.java
@@ -0,0 +1,7 @@
+import java.time.year;
+public class Leapyear {
+    public static void main(String[] args) {
+        Year y = Year.now();
+        System.out.println(y);
+    }
+}
---
+++ src/Twostringsequal.java
@@ -0,0 +1,11 @@
+public class Twostringsequal {
+    public static void main(String args[]){
+        String s1="Nitu";
+        String s2="Nitu";
+        String s3="Nitu";
+        String s4="Archana";
+        System.out.println(s1.equals(s2));//true
+        System.out.println(s1.equals(s3));//true
+        System.out.println(s1.equals(s4));//false
+    }
+}
